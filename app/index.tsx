384ecf0d97dd0c0cc4bb9f6f4eb6a3d1dccfd823
macdee501 <62038430+macdee501@users.noreply.github.com>
2026-02-13 19:04:10 +0200

    added boiler components

--- app/index.tsx
+++ app/index.tsx
@@ -1,15 +1,24 @@
-import { Text, View } from "react-native";
+import CategoryList from "@/components/home/Categories/CategoryList";
+import Favourites from "@/components/home/Favourites";
+import HeroCarousel from "@/components/home/HeroCarousel";
+import SearchMenu from "@/components/home/SearchMenu";
+import Footer from "@/components/layout/Footer";
+import Header from "@/components/layout/Header";
+import { ScrollView, Text, View } from "react-native";
+import { SafeAreaView } from "react-native-safe-area-context";
 
 export default function Index() {
   return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-      }}
-    >
-      <Text>Edit app/index.tsx to edit this screen.</Text>
-    </View>
+    <SafeAreaView style={{flex:1}}>
+      <ScrollView>
+        <Header/>
+        <HeroCarousel/>
+        <SearchMenu/>
+        <Favourites/>
+        <CategoryList/>
+        <Footer/>
+      </ScrollView>
+    </SafeAreaView>
+    
   );
 }
